29ae2972d9ed1edd81ca9c90e4efb5465593b7e8
thiagomaes <thiago.l.maes@gmail.com>
2026-05-20 00:13:00 -0300

    Adicionando animações do jogador e script de movimentação

--- Scripts/jogador.gd
+++ Scripts/jogador.gd
@@ -1,23 +1,38 @@
 class_name Jogador extends CharacterBody2D
 
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO
+var state : String = "idle"
 var move_speed : float = 100.0
 
-# Called when the node enters the scene tree for the first time.
+@onready var anim = $AnimatedSprite2D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	anim.play("idle_front")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process( delta ):
-	
-	var direction : Vector2 = Vector2.ZERO
-	
+func _process(delta):
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	velocity = direction * move_speed
+	if direction != Vector2.ZERO:
+		cardinal_direction = direction.normalized()
+		velocity = direction * move_speed
+		
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				state = "walk_right"
+			else:
+				state = "walk_left"
+		else:
+			if direction.y > 0:
+				state = "walk_front"
+			else:
+				state = "walk_back"
+	else:
+		velocity = Vector2.ZERO
+		state = state.replace("walk_", "idle_").replace("run_", "idle_")
 	
-	pass
-	
-func _physics_process( delta ):
+	anim.play(state)
+
+func _physics_process(delta):
 	move_and_slide()
